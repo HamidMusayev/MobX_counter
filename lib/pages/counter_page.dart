a8629d8599c84033d19ffb7fd789c50faf5dc2dd
Hamid Musayev <hemidvsmusayev@gmail.com>
2020-07-29 13:46:48 +0400

    initial commit

--- lib/pages/counter_page.dart
+++ lib/pages/counter_page.dart
@@ -1,31 +1,37 @@
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter/material.dart';
+import 'package:mobx_app/store/counter/counter.dart';
 
 class CounterPage extends StatelessWidget {
+  final Counter counter = Counter();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Flutter MobX"),
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Text("Counter", style: TextStyle(fontSize: 30)),
-            Text("0", style: TextStyle(fontSize: 30)),
-            Row(
-              children: <Widget>[
-                FlatButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.add),
-                    label: Text("Add")),
-                FlatButton.icon(
-                    onPressed: () {},
-                    icon: Icon(Icons.remove),
-                    label: Text("Remove"))
-              ],
-            )
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("Counter", style: TextStyle(fontSize: 40)),
+          Observer(
+              builder: (_) => Text("$counter",
+                  style: TextStyle(fontSize: 30, color: Colors.blueGrey))),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton.icon(
+                  onPressed: counter.increment,
+                  icon: Icon(Icons.add),
+                  label: Text("Add")),
+              FlatButton.icon(
+                  onPressed: counter.decrement,
+                  icon: Icon(Icons.remove),
+                  label: Text("Remove"))
+            ],
+          )
+        ],
       ),
     );
   }
